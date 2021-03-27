e55a790dcd7245bf0bcf5cf54e2236602655c333
Andrey Cherniak <baza904@gmail.com>
2021-03-27 17:33:53 +0200

    add completed hw

---
+++ index.js
@@ -0,0 +1,41 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+  body: document.querySelector('body'),
+};
+let isActive = false;
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', startSwitcher);
+refs.stopBtn.addEventListener('click', stopSwitcher);
+
+function startSwitcher() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  intervalId = setInterval(colorSwitcher, 1000);
+}
+
+function randomIntegerFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function stopSwitcher() {
+  isActive = false;
+  clearInterval(intervalId);
+}
+
+function colorSwitcher() {
+  const randomInteger = randomIntegerFromInterval(0, 5);
+  refs.body.style.backgroundColor = `${colors[randomInteger]}`;
+}
